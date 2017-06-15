01a75d066b4beed15833fcccedb2ef5e37eeee77
sky1508 <sky12345085@yahoo.com>
2017-06-15 20:21:46 +0530

    Added Example of Routing using angular

---
+++ ang_route_example/js/app.js
@@ -0,0 +1,34 @@
+﻿var app = angular.module("Demo", ["ngRoute"])
+            .config(function ($routeProvider) {
+                $routeProvider
+                .when("/home", {
+                    templateUrl: "Templates/home.html",
+                    controller: "homeController"
+                })
+
+            .when("/courses", {
+                templateUrl: "Templates/courses.html",
+                controller: "coursesController"
+            })
+
+            .when("/students", {
+                templateUrl: "Templates/student.html",
+                controller: "studentController"
+            })
+			
+			.otherwise({
+                redirectTo: "/home" 
+            })
+            })
+
+        .controller("homeController", function ($scope) {
+            $scope.message = "Home Page";
+        })
+
+        .controller("coursesController", function ($scope) {
+            $scope.courses = ["C#", "CPP", "Java", "MySQL", "HTML", "Angular"];
+        })
+
+        .controller("studentController", function ($scope) {
+            $scope.students = ["AKK", "VVV", "BBB", "NNQL", "GFF", "EER"];
+        });
