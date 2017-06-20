bd9efec73cf7551c6a14ffb205b96b0ec0f1a5be
sky1508 <sky12345085@yahoo.com>
2017-06-20 19:31:16 +0530

    Angular app in VS for Routing Params

---
+++ ang_VS_Routeparams/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Routeparams")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CTS")]
+[assembly: AssemblyProduct("Routeparams")]
+[assembly: AssemblyCopyright("Copyright © CTS 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fe8bd3bf-19c1-45a0-895f-e35adfe32a5b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ang_VS_Routeparams/js/app.js
@@ -0,0 +1,60 @@
+﻿/// <reference path="angular-route.js" />
+/// <reference path="angular.min.js" />
+
+var app = angular.module("Demo", ["ngRoute"])
+
+            .config(function ($routeProvider, $locationProvider) {
+                $routeProvider
+                .when("/home", {
+                    templateUrl: "Templates/home.html",
+                    controller: "homeController"
+                })
+
+            .when("/courses", {
+                templateUrl: "Templates/courses.html",
+                controller: "coursesController"
+            })
+
+            .when("/employee", {
+                templateUrl: "Templates/employee.html",
+                controller: "employeeController"
+            })
+
+            .when("/employees/:id", {
+                templateUrl: "Templates/employeeDetails.html",
+                controller: "employeeDetailsController"
+            })
+
+            .otherwise({
+                redirectTo: "/home"
+            })
+                //$locationProvider.html5Mode(true);
+            })
+
+
+
+        .controller("homeController", function ($scope) {
+            $scope.message = "Home Page";
+        })
+
+        .controller("coursesController", function ($scope) {
+            $scope.courses = ["C#", "CPP", "Java", "MySQL", "HTML", "Angular"];
+        })
+
+        .controller("employeeController", function ($scope, $http) {
+            $http.get('EmployeeService.asmx/GetAllEmployees')
+                    .then(function (response) {
+                        $scope.employees = response.data;
+                    });
+        })
+
+        .controller("employeeDetailsController", function ($scope, $http, $routeParams) {
+            $http({
+                url: "EmployeeService.asmx/GetEmployee",
+                params: {id:$routeParams.id},
+                method: "get"
+            })
+            .then(function (response) {
+                $scope.employee = response.data;
+             });
+        });
