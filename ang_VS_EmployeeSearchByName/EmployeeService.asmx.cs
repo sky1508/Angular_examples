e8ba2ad4daf7b2602d4ab1db26486779c8ea77f8
sky1508 <sky12345085@yahoo.com>
2017-06-20 19:26:49 +0530

    Angular App to get employee by using name from DB

---
+++ ang_VS_EmployeeSearchByName/EmployeeService.asmx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Script.Serialization;
+
+namespace EmployeeSearchByName
+{
+    /// <summary>
+    /// Summary description for EmployeeService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class EmployeeService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public void GetAllEmployees()
+        {
+            List<Employee> listEmployees = new List<Employee>();
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("select * from dbo.Test1;", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.id = Convert.ToInt32(rdr["id"]);
+                    employee.name = rdr["name"].ToString();
+                    employee.gender = rdr["gender"].ToString();
+                    employee.salary = Convert.ToInt32(rdr["salary"]);
+                    listEmployees.Add(employee);
+                }
+            }
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(listEmployees));
+        }
+
+        [WebMethod]
+        public void GetEmployee(int id)
+        {
+            Employee employee = new Employee();
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("select * from dbo.Test1 where id = @id;", con);
+                SqlParameter param = new SqlParameter()
+                {
+                    ParameterName = "@id",
+                    Value = id
+                };
+
+                cmd.Parameters.Add(param);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    employee.id = Convert.ToInt32(rdr["id"]);
+                    employee.name = rdr["name"].ToString();
+                    employee.gender = rdr["gender"].ToString();
+                    employee.salary = Convert.ToInt32(rdr["salary"]);
+                }
+            }
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(employee));
+        }
+
+        [WebMethod]
+        public void GetEmployeeByName(string name)
+        {
+            List<Employee> listEmployees = new List<Employee>();
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("select * from dbo.Test1 where name like @name;", con);
+                SqlParameter param = new SqlParameter()
+                {
+                    ParameterName = "@name",
+                    Value = name + "%"
+                };
+
+                cmd.Parameters.Add(param);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.id = Convert.ToInt32(rdr["id"]);
+                    employee.name = rdr["name"].ToString();
+                    employee.gender = rdr["gender"].ToString();
+                    employee.salary = Convert.ToInt32(rdr["salary"]);
+                    listEmployees.Add(employee);
+                }
+            }
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(listEmployees));
+        }
+    }
+}
---
+++ ang_VS_EmployeeSearchByName/js/app.js
@@ -0,0 +1,144 @@
+﻿/// <reference path="angular-route.js" />
+/// <reference path="angular.min.js" />
+
+var app = angular.module("Demo", ["ngRoute"])
+
+            .config(function ($routeProvider, $locationProvider) {
+                $routeProvider
+                .when("/home", {
+                    templateUrl: "Templates/home.html",
+                    controller: "homeController as homeCtrl"
+                })
+
+            .when("/courses", {
+                templateUrl: "Templates/courses.html",
+                controller: "coursesController",
+                controllerAs: "coursesCtrl"
+            })
+
+            .when("/employee", {
+                templateUrl: "Templates/employee.html",
+                controller: "employeeController",
+                controllerAs: "employeeCtrl"
+            })
+
+            .when("/employees/:id", {
+                templateUrl: "Templates/employeeDetails.html",
+                controller: "employeeDetailsController as employeeDetailsCtrl"
+            })
+
+            .when("/employeesSearch/:name?", {
+                templateUrl: "Templates/employeeSearch.html",
+                controller: "employeeSearchController as employeeSearchCtrl"
+            })
+
+            .otherwise({
+                redirectTo: "/home"
+            })
+                //$locationProvider.html5Mode(true);
+            })
+
+
+
+        .controller("homeController", function () {
+            this.message = "Home Page";
+        })
+
+        .controller("coursesController", function () {
+            this.courses = ["C#", "CPP", "Java", "MySQL", "HTML", "Angular"];
+        })
+
+        .controller("employeeController", function ($http, $route, $rootScope, $log, $scope, $location) {
+            var that = this;
+
+            that.searchEmployee = function () {
+                if (that.name) {
+                    $location.url("/employeesSearch/" + that.name);
+                }
+                else {
+                    $location.url("/employeesSearch");
+                }
+            }
+
+
+
+            //various events fired on route change. See the console in verbose mode in browser to observe changes
+            $rootScope.$on("$locationChangeStart", function (event, next, current) {
+                $log.debug("$locationChangeStart fired");
+                $log.debug(event);
+                $log.debug(next);
+                $log.debug(current);
+            });
+
+            $rootScope.$on("$routeChangeStart", function (event, next, current) {
+                $log.debug("$routeChangeStart fired");
+                $log.debug(event);
+                $log.debug(next);
+                $log.debug(current);
+            });
+
+            /*$rootScope.$on("$locationChangeSuccess", function () {
+            $log.debug("$locationChangeSuccess fired");
+            });
+
+            $rootScope.$on("$locationChangeSuccess", function () {
+            $log.debug("$locationChangeSuccess fired");
+            });*/
+            /*$scope.$on("$routeChangeStart", function (event, next, current) {
+            if (!confirm("Are you sure you want to navigate away from this page to " + next.$$route.originalPath)) {
+            event.preventDefault();
+            }
+            })*/
+
+            $scope.$on("$locationChangeStart", function (event, next, current) {
+                if (!confirm("Are you sure you want to navigate away from this page to " + next)) {
+                    event.preventDefault();
+                }
+            })
+
+
+            that.reloadData = function () {
+                $route.reload();
+            }
+            $http.get('EmployeeService.asmx/GetAllEmployees')
+                    .then(function (response) {
+                        that.employees = response.data;
+                    });
+        })
+
+        .controller("employeeDetailsController", function ($http, $routeParams) {
+            var that = this;
+            $http({
+                url: "EmployeeService.asmx/GetEmployee",
+                params: { id: $routeParams.id },
+                method: "get"
+            })
+            .then(function (response) {
+                that.employee = response.data;
+            });
+        })
+
+        .controller("employeeSearchController", function ($http, $routeParams) {
+            var that = this;
+
+            if ($routeParams.name) {
+                $http({
+                    url: "EmployeeService.asmx/GetEmployeeByName",
+                    params: { name: $routeParams.name },
+                    method: "get"
+                })
+                .then(function (response) {
+                    that.employees = response.data;
+             
+                })
+            }
+            else {
+                $http.get('EmployeeService.asmx/GetAllEmployees')
+                    .then(function (response) {
+                        that.employees = response.data;
+                    })
+            }
+
+
+
+        });
