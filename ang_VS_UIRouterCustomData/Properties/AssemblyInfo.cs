5b7d244589c92d847606302987a12be6bf57f3c7
sky1508 <sky12345085@yahoo.com>
2017-06-20 19:36:04 +0530

    Angular app in VS - UI ROuter for custom data

---
+++ ang_VS_UIRouterCustomData/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UIRouterCustomData")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CTS")]
+[assembly: AssemblyProduct("UIRouterCustomData")]
+[assembly: AssemblyCopyright("Copyright © CTS 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("613ce155-6e9a-4b84-b82e-397713901b1a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ang_VS_UIRouterCustomData/js/app.js
@@ -0,0 +1,121 @@
+﻿/// <reference path="angular-route.js" />
+/// <reference path="angular.min.js" />
+
+var app = angular.module("Demo", ["ui.router"])
+
+            .config(function ($stateProvider, $locationProvider, $urlMatcherFactoryProvider, $urlRouterProvider) {
+
+                $urlRouterProvider.otherwise("/home");
+                $urlMatcherFactoryProvider.caseInsensitive(true);
+                $stateProvider
+                .state("home", {
+                    url: "/home",
+                    templateUrl: "Templates/home.html",
+                    controller: "homeController",
+                    controllerAs: "homeCtrl",
+                    data: {
+                        customData1: "Home State Custom Data 1",
+                        customData2: "Home State Custom Data 2"
+                    }
+                })
+
+            .state("courses", {
+                url: "/courses",
+                templateUrl: "Templates/courses.html",
+                controller: "coursesController",
+                controllerAs: "coursesCtrl",
+                data: {
+                    customData1: "Courses State Custom Data 1",
+                    customData2: "Courses State Custom Data 2"
+                }
+            })
+
+            .state("employee", {
+                url: "/employee",
+                templateUrl: "Templates/employee.html",
+                controller: "employeeController",
+                controllerAs: "employeeCtrl"
+            })
+
+            .state("employeeDetails", {
+                url: "/employees/:id",
+                templateUrl: "Templates/employeeDetails.html",
+                controller: "employeeDetailsController",
+                controllerAs: "employeeDetailsCtrl"
+            })
+
+             .state("employeesSearch", {
+                 url: "/employeesSearch/:name",
+                 templateUrl: "Templates/employeeSearch.html",
+                 controller: "employeeSearchController",
+                 controllerAs: "employeeSearchCtrl"
+             })
+
+            })
+
+
+
+        .controller("homeController", function ($state) {
+            this.message = "Home Page";
+            this.homeCustomData1 = $state.current.data.customData1;
+            this.homeCustomData2 = $state.current.data.customData2;
+
+            this.coursesCustomData1 = $state.get("courses").data.customData1;
+            this.coursesCustomData2 = $state.get("courses").data.customData2;
+        })
+
+        .controller("coursesController", function () {
+            this.courses = ["C#", "CPP", "Java", "MySQL", "HTML", "Angular"];
+        })
+
+        .controller("employeeController", function ($http, $state) {
+            var that = this;
+
+            that.searchEmployee = function () {
+                $state.go("employeesSearch", { name: that.name });
+            }
+
+            that.reloadData = function () {
+                $state.reload();
+            }
+            $http.get('EmployeeService.asmx/GetAllEmployees')
+                    .then(function (response) {
+                        that.employees = response.data;
+                    });
+        })
+
+        .controller("employeeDetailsController", function ($http, $stateParams) {
+            var that = this;
+            $http({
+                url: "EmployeeService.asmx/GetEmployee",
+                params: { id: $stateParams.id },
+                method: "get"
+            })
+            .then(function (response) {
+                that.employee = response.data;
+            });
+        })
+
+         .controller("employeeSearchController", function ($http, $stateParams) {
+             var that = this;
+
+             if ($stateParams.name) {
+                 $http({
+                     url: "EmployeeService.asmx/GetEmployeeByName",
+                     params: { name: $stateParams.name },
+                     method: "get"
+                 })
+                .then(function (response) {
+                    that.employees = response.data;
+                })
+             }
+             else {
+                 $http.get('EmployeeService.asmx/GetAllEmployees')
+                    .then(function (response) {
+                        that.employees = response.data;
+                    })
+             }
+
+
+
+         });
